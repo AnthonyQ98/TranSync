cc30a6bd5ba66da74d81a72e7a7e74b2328ce83c
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-03 21:30:20 +0100

    implementing public_transport_server.js code & installing required node modules

---
+++ src/servers/public_transport_server.js
@@ -0,0 +1,23 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinitionTraffic = protoLoader.loadSync('traffic_lights.proto');
+const trafficLightsProto = grpc.loadPackageDefinition(packageDefinitionTraffic).TrafficLights;
+
+const trafficLightsClient = new trafficLightsProto('localhost:50051', grpc.credentials.createInsecure());
+
+const request = {
+    intersection_id: "AnthonysAmazingIntersection",
+    signal_timings: [
+        { color: "GREEN", duration_seconds: 30 },
+        { color: "RED", duration_seconds: 20 },
+    ]
+};
+
+trafficLightsClient.changeSignalTimings({ request }, (err, response) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log('Response:', response);
+    }
+});
