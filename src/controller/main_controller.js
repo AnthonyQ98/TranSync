29ff2dc08009e9697f1eb53e5f59bcabea6756b1
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-03 22:17:15 +0100

    Finally able to access request body.. grpc has weird conventions, solution found here: https://stackoverflow.com/questions/70493588/grpc-grpc-js-empty-request-received-at-the-backend-service-from-client

--- src/controller/main_controller.js
+++ src/controller/main_controller.js
@@ -1,21 +1,22 @@
 const grpc = require('@grpc/grpc-js');
 const protoloader = require('@grpc/proto-loader');
 const packageDefinition = protoloader.loadSync('protos/traffic_lights.proto', {});
-const trafficLightsProto = grpc.loadPackageDefinition(packageDefinition).TrafficLights;
+const trafficLightsProto = grpc.loadPackageDefinition(packageDefinition).trafficlights;
 
 
 function main() {
     const client = new trafficLightsProto.TrafficLights('localhost:50051', grpc.credentials.createInsecure());
 
-    const request = {
-        intersection_id: "AnthonysAmazingIntersection",
-        signal_timings: [
-            { color: "GREEN", duration_seconds: 30 },
-            { color: "RED", duration_seconds: 20 },
+    const body = {
+        intersectionId: "AnthonysAmazingIntersection",
+        signalTimings: [
+            { color: "GREEN", durationSeconds: 30 },
+            { color: "RED", durationSeconds: 20 },
         ]
     };
+    console.log(body)
 
-    client.ChangeSignalTimings(request, function(err, response) {
+    client.ChangeSignalTimings(body, function(err, response) {
         if (err) {
             console.error(err);
         } else {
--- src/servers/traffic_lights_server.js
+++ src/servers/traffic_lights_server.js
@@ -1,16 +1,11 @@
 const grpc = require('@grpc/grpc-js');
 const protoloader = require('@grpc/proto-loader');
 const packageDefinition = protoloader.loadSync('protos/traffic_lights.proto', {});
-const trafficLightsProto = grpc.loadPackageDefinition(packageDefinition).TrafficLights;
+const trafficLightsProto = grpc.loadPackageDefinition(packageDefinition).trafficlights;
 
-function changeSignalRequest(call, callback) {
-    const result = call.request.number1 + call.request.number2;
-    callback(null, {result});
-}
 
 function changeSignalTimings(call, callback) {
     const request = call.request;
-    
     console.log('Received request:', request);
 
     const response = {
@@ -26,12 +21,10 @@ const serverAddr = '0.0.0.0:50051';
 
 // Add all services in a single object
 const services = {
-    ChangeSignalRequest: changeSignalRequest,
     ChangeSignalTimings: changeSignalTimings
 };
 server.addService(trafficLightsProto.TrafficLights.service, services);
 
 server.bindAsync(serverAddr, grpc.ServerCredentials.createInsecure(), () => {
-    server.start()
     console.log("Server running at " + serverAddr)
 });
