5e5a2f611ea4d139a81090c586fb550d70e849b5
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-04 23:02:23 +0100

    starting to implement CLI user interface to interact with services - first implementation of traffic lights cli interface

--- src/controller/main_controller.js
+++ src/controller/main_controller.js
@@ -1,5 +1,9 @@
 const grpc = require('@grpc/grpc-js');
 const protoloader = require('@grpc/proto-loader');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 const trafficLightsPackageDefinition = protoloader.loadSync('protos/traffic_lights.proto', {});
 const trafficLightsProto = grpc.loadPackageDefinition(trafficLightsPackageDefinition).trafficlights;
 
@@ -9,30 +13,64 @@ const publicTransportProto = grpc.loadPackageDefinition(publicTransportPackageDe
 const parkingPackageDefinition = protoloader.loadSync('protos/parking.proto', {});
 const parkingProto = grpc.loadPackageDefinition(parkingPackageDefinition).Parking;
 
+function showMainMenu() {
+    console.log("Transport Synchronization System:");
+    console.log("1) Traffic Lights");
+    console.log("2) Traffic");
+    console.log("3) Public Transport");
+}
 
-function main() {
+
+async function handleTrafficLights() {
     const client = new trafficLightsProto.TrafficLights('localhost:50051', grpc.credentials.createInsecure());
+    const intersectionId = await new Promise((resolve) => readline.question('Enter intersection ID: ', resolve));
+    const greenDuration = await new Promise((resolve) => readline.question('Enter duration for GREEN signal (in seconds): ', resolve));
+    const redDuration = await new Promise((resolve) => readline.question('Enter duration for RED signal (in seconds): ', resolve));
+
     const body = {
-        intersectionId: "AnthonysAmazingIntersection",
+        intersectionId: intersectionId,
         signalTimings: [
-            { color: "GREEN", durationSeconds: 30 },
-            { color: "RED", durationSeconds: 20 },
+            { color: "GREEN", durationSeconds: parseInt(greenDuration) },
+            { color: "RED", durationSeconds: parseInt(redDuration) },
         ]
     };
-    client.ChangeSignalTimings(body, function(err, response) {
+
+    client.ChangeSignalTimings(body, function (err, response) {
         if (err) {
             console.error(err);
         } else {
-            console.log('Change Signal Timing result:', response);
+            console.log(response.message);
         }
-    })
+    });
+}
+
+
+function main() {
+    showMainMenu();
+
+    readline.question('Enter your choice: ', async function (choice) {
+        switch (choice) {
+            case '1':
+                await handleTrafficLights();
+                break;
+            case '2':
+                await handleTraffic();
+                break;
+            case '3':
+                await handlePublicTransport();
+                break;
+            default:
+                console.log("Invalid choice");
+        }
+        readline.close();
+    });
 
 
     const client2 = new publicTransportProto.PublicTransport('localhost:50051', grpc.credentials.createInsecure());
     const body2 = {
         busStopId: "12"
     };
-    client2.GetNextBus(body, function(err, response) {
+    client2.GetNextBus(body2, function (err, response) {
         if (err) {
             console.error(err);
         } else {
@@ -49,14 +87,14 @@ function main() {
         parkingLotId: "NCI_STAFF_PARKING",
         numSpots: 12
     };
-    client3.CheckAvailability(body3_1, function(err, response) {
+    client3.CheckAvailability(body3_1, function (err, response) {
         if (err) {
             console.error(err);
         } else {
             console.log('Checking availability of parking at ' + body3_2.parkingLotId + ':', JSON.stringify(response.availableSpots) + ' spots remaining.');
         }
     })
-    client3.ReserveSpot(body3_2, function(err, response) {
+    client3.ReserveSpot(body3_2, function (err, response) {
         if (err) {
             console.error(err);
         } else {
--- src/services/traffic_lights.js
+++ src/services/traffic_lights.js
@@ -3,9 +3,10 @@ function changeSignalTimings(call, callback) {
     console.log('Received request:', request);
 
     const response = {
-        message: "Signal timings changed successfully"
+        message: "------\nChanged intersection " + request.intersectionId + "\n" + request.signalTimings[0].color + " light duration (seconds) set to: " + request.signalTimings[0].durationSeconds + "\n" + request.signalTimings[1].color + " light duration (seconds) set to: " + request.signalTimings[1].durationSeconds + "\n------"
     };
     callback(null, response);
 }
+
 
 module.exports = { changeSignalTimings };
