261317e00e23885275ef3a2b9c06088050b4b1f1
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-05 21:22:53 +0100

    Adding in traffic light validation

--- src/services/traffic_lights.js
+++ src/services/traffic_lights.js
@@ -2,8 +2,14 @@ function changeSignalTimings(call, callback) {
     const request = call.request;
     console.log('Received request:', request);
 
+    let msg;
+    if (request.intersectionId == "") {
+        msg = "Invalid intersection - can not change traffic lights!"
+    } else {
+        msg = "------\nChanged intersection " + request.intersectionId + "\n" + request.signalTimings[0].color + " light duration (seconds) set to: " + request.signalTimings[0].durationSeconds + "\n" + request.signalTimings[1].color + " light duration (seconds) set to: " + request.signalTimings[1].durationSeconds + "\n------"
+    }
     const response = {
-        message: "------\nChanged intersection " + request.intersectionId + "\n" + request.signalTimings[0].color + " light duration (seconds) set to: " + request.signalTimings[0].durationSeconds + "\n" + request.signalTimings[1].color + " light duration (seconds) set to: " + request.signalTimings[1].durationSeconds + "\n------"
+        message: msg
     };
     callback(null, response);
 }
