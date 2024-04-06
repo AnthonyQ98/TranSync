2f41e03e603cad9f3e0ff760ea9482c35dac8fd2
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-06 13:16:01 +0100

    Adding comments for traffic lights proto and service related code

--- src/services/traffic_lights.js
+++ src/services/traffic_lights.js
@@ -1,23 +1,31 @@
+// Function to handle changing signal timings
 function changeSignalTimings(call, callback) {
     try {
+        // Extract the request from the gRPC call
         const request = call.request;
+        // Log the received request for debugging purposes
         console.log('Received request:', request);
 
         let msg;
+        // Check if the intersection ID is empty
         if (request.intersectionId == "") {
-            msg = "Invalid intersection - can not change traffic lights!"
+            // If the intersection ID is empty, indicate that the intersection is invalid and cannot change traffic lights
+            msg = "Invalid intersection - cannot change traffic lights!"
         } else {
+            // If the intersection ID is not empty, construct a message indicating the changed signal timings
             msg = "------\nChanged intersection " + request.intersectionId + "\n" + request.signalTimings[0].color + " light duration (seconds) set to: " + request.signalTimings[0].durationSeconds + "\n" + request.signalTimings[1].color + " light duration (seconds) set to: " + request.signalTimings[1].durationSeconds + "\n------"
         }
+        // Prepare the response with the message indicating the status of the signal timing change operation
         const response = {
             message: msg
         };
+        // Send the response back to the client
         callback(null, response);
     } catch (error) {
+        // Handle any errors that occur during processing
         console.error('Error handling traffic lights:', error.message)
     }
-
 }
 
-
+// Export the changeSignalTimings function to make it accessible from other modules
 module.exports = { changeSignalTimings };
